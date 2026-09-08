aeed135d5979650d280be67a2a621eb01e28b77b
H7-25 <admin@simosnap.org>
2026-09-08 09:50:59 +0200

    Fix Edition entity for fresh installs

--- app/Entities/Edition.php
+++ app/Entities/Edition.php
@@ -2,3 +2,24 @@
 declare(strict_types=1);
 
 namespace Monoverse\Entities;
+
+class Edition
+{
+public function __construct(
+public readonly string $id,
+public readonly string $name,
+public readonly string $description,
+public readonly string $version,
+public readonly string $author = '',
+public readonly string $website = '',
+public readonly string $icon = '',
+public readonly array $modules = [],
+public readonly string $status = 'available'
+) {
+}
+
+public function isAvailable(): bool
+{
+return $this->status === 'available';
+}
+}

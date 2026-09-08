bc6c016d923f857e517b85218517627b7493b193
H7-25 <admin@simosnap.org>
2026-09-08 10:01:16 +0200

    Restore default theme

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ return [
     'app_name' => 'Monoverse',
     'app_tagline' => 'IRC community websites by SimosNap',
     'base_url' => 'https://monoverse.org',
-    'theme' => 'social',
+    'theme' => 'default',
     'debug' => true,
 
     'infinite_scroll_page_size' => 5,
